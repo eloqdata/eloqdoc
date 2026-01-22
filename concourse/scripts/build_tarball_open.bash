122a5bb9cae8226daec7ff27afc840718e2622b8
Jerry Zhao <ch3nzhao@gmail.com>
2026-01-22 22:37:45 +0800

    chore: update concourse checkout scripts (#440)

--- concourse/scripts/build_tarball_open.bash
+++ concourse/scripts/build_tarball_open.bash
@@ -25,7 +25,7 @@ fi
 cd "$HOME"
 ln -sfn "${WORKSPACE}/eloqdoc_src" eloqdoc
 cd eloqdoc
-ln -sfn "$WORKSPACE/logservice_src" src/mongo/db/modules/eloq/log_service
+ln -sfn "$WORKSPACE/logservice_src" src/mongo/db/modules/eloq/data_substrate/log_service
 pushd src/mongo/db/modules/eloq/data_substrate/tx_service
 ln -sfn "$WORKSPACE/raft_host_manager_src" raft_host_manager
 popd
